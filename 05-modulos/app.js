7aaee5eb98960a6f90e3d0125d69bcd706cd47ba
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2023-09-02 13:41:52 -0300

    feat: módulos

---
+++ 05-modulos/app.js
@@ -0,0 +1,16 @@
+var soma = require("./calculadora");
+
+
+function mult(a,b){
+    return a / b; 
+}
+
+function sub(a,b){
+    return a / b; 
+}
+
+function div(a,b){
+    return a / b; 
+}
+
+console.log(soma(10,20))
